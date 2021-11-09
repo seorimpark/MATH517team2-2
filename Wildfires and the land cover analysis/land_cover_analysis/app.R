5abf8247e008ea3e6ee94b5310091f9f0c5c475b
Agnaou Zineb <agnaou.zineb@gmail.com>
2021-11-09 13:34:15 +0100

    I now wanted to work on the app, I found it hard because we have a enormous amount of data and when doing a shiny app it took a long time to load. I then tried to generate jpeg images first and then loading them into the shiny app, I started doing it yesterday but after 2 hours I was not eneven at half of the 5520 iterations and thus plot I had to generate. I am not sure how to display them for now, I am thinking about it. I kept the analysis done prior

--- Wildfires and the land cover analysis/land_cover_analysis/app.R
+++ Wildfires and the land cover analysis/land_cover_analysis/app.R
@@ -14,119 +14,24 @@ library(zoo)
 library(ggplot2)
 library(hrbrthemes) 
 library(corrplot)
-
-
-# loadind data 
-load("data_train_DF.RData")
-data_withNA = data_train_DF
-
-#removing NA
-data<-data_withNA %>% drop_na()
-
-
-    
-
-filter_year<-function(data,year){
-    if(year %in% unique(data$year)){
-        data_filtered<-data %>% filter(year == year)
-        data_filtered$Date<-as.yearmon(paste(data_filtered$year, data_filtered$month), "%Y %m")
-        data_filtered$Date<-as.Date(data_filtered$Date)
-        data_filtered<-data_filtered[ , -which(names(data_filtered) %in% c("year","month"))]
-        return (data_filtered)}
-    else return(data)}
-
-
-data93<-filter_year(data,1993)
-data94<-filter_year(data,1994)
-data95<-filter_year(data,1995)
-data96<-filter_year(data,1996)
-data97<-filter_year(data,1997)
-data98<-filter_year(data,1998)
-data99<-filter_year(data,1999)
-data00<-filter_year(data,2000)
-data01<-filter_year(data,2001)
-data02<-filter_year(data,2002)
-data03<-filter_year(data,2003)
-data04<-filter_year(data,2004)
-data05<-filter_year(data,2005)
-data06<-filter_year(data,2006)
-data07<-filter_year(data,2007)
-data08<-filter_year(data,2008)
-data09<-filter_year(data,2009)
-data10<-filter_year(data,2010)
-data11<-filter_year(data,2011)
-data12<-filter_year(data,2012)
-data13<-filter_year(data,2013)
-data14<-filter_year(data,2014)
-data15<-filter_year(data,2015)
-
-data_all<-data
-data_all$Date <- as.yearmon(paste(data_all$year, data_all$month), "%Y %m")
-data_all$Date<-as.Date(data_all$Date)
-data_all<-data_all[ , -which(names(data_all) %in% c("year","month"))]
-
-lats<-unique(data_all$lat)
-lons<-unique(data_all$lon)
-lcs<-1:17
-names_lc <- c('cropland rainfed',
-              'cropland rainfed herbaceous cover',
-              'mosaic cropland',
-              'mosaic natural vegetation',
-              'tree broadleaved evergreen closed to open',
-              'tree broadleaved deciduous closed to open',
-              'tree needleleave evergreen closed to open',
-              'tree needleleaved deciduous closed to open',
-              'tree mixed',
-              'mosaic tree and shrub',
-              'shrubland',
-              'grassland',
-              'sparse vegetation',
-              'tree cover flooded fresh or brakish water',
-              'shrub or herbaceous cover flooded',
-              'urban',
-              'bare areas',
-              'water')
-
-min_lon=min(data93$lon)
-max_lon=max(data93$lon)
-min_lat=min(data93$lat)
-max_lat=max(data93$lat)
-
-# plots to know which place in the map we are talking
-plot(data93$lon, data93$lat,pch=9,xlab=" ",lwd=2,cex = .5, ylab="", xlim=c(min_lon,max_lon ), ylim=c(min_lat, max_lat))
-par(new=TRUE)
-plot(-95.25, 49.25, col="red",pch="O",xlab=" ",lwd=5, ylab="", xlim=c(min_lon,max_lon ), ylim=c(min_lat, max_lat))
+library(png)
+library(imager)
 
 
 
-# from one lon and lat cordinates, it gives you the values of lc you want if exits
-# lc goes from 1 to 18
-lc_from_place<-function(df, lon_,lat_, lc_)
-{
-    if (lon_ %in% lons && lat_ %in% lats && lc_ %in% lcs)
-    {
-        values<- df %>% filter(lat == lat_) %>% filter(lon == lon_) 
-        col_index<-5+lc_
-        values<-values[, c(col_index,36)]
-        return(values)
-        
-    }
-    else{warning("wrong indices")}
-}
 
-#for(lats_ in lats ){
-#lats_ = lats[1]
-#for(lons_ in lons){
-#lons_= lons[1]
-#df <- NULL
-# for(lcs_ in 1:17)
-# {trial1<-lc_from_place(data_all, lons_,lats_, lcs_)
-# temp_df <- data.frame(x=trial1[,2], y=trial1[,1], col=rep(lcs_:lcs_, each=nrow(trial1)))
-# df <- rbind(df,temp_df)} 
-# p=ggplot(df,aes(x=x,y=y,group=col,colour=factor(col))) +geom_line()
-# p=p+guides(colour=guide_legend(ncol=2))
-# p
-#}
+
+
+
+
+
+
+
+
+
+
+
+
 
 # unique latitudes:
 
@@ -149,7 +54,7 @@ ui <- shinyUI(fluidPage(
             step = 0.5,
             value= 28.75,
             ticks = FALSE,
-            animate = TRUE
+            animate = FALSE
         ),
         sliderInput(
             "lon", "Select the longitude",
@@ -158,12 +63,12 @@ ui <- shinyUI(fluidPage(
             step = 0.5,
             value= -70.25,
             ticks = FALSE,
-            animate = TRUE
+            animate = FALSE
         )),
         
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("Land cover")
+            plotOutput("myPlot")
         )
     ))
 
@@ -172,27 +77,24 @@ ui <- shinyUI(fluidPage(
 
 server <- function(input, output) {
     
-         output$myPlot <- renderPlot({
+            output$myPlot <- renderPlot({
             distLat <- input$lat
             distLon <- input$lon
-            if(disLat != 25.25 &&disLat != 49.25) {disLat<-disLat-0.05}
-            if(disLon != -124.25 &&disLat != -66.75) {disLon<-disLon-0.05}
+            if(distLat != 25.25 &&distLat != 49.25) {distLat<-distLat-0.05}
+            if(distLon != -124.25 &&distLat != -66.75) {distLon<-distLon-0.05}
+            print(getwd())
             
-            plot(data93$lon, data93$lat,pch=9,xlab=" ",lwd=2,cex = .5, ylab="", xlim=c(min_lon,max_lon ), ylim=c(min_lat, max_lat))
-            par(new=TRUE)
-            plot(distLon, distLat, col="red",pch="O",xlab=" ",lwd=5, ylab="", xlim=c(min_lon,max_lon ), ylim=c(min_lat, max_lat))
-            
-            # for(lcs_ in 1:17)
-            # {trial1<-lc_from_place(data_all, disLon ,disLat, lcs_)
-            # temp_df <- data.frame(x=trial1[,2], y=trial1[,1], col=rep(lcs_:lcs_, each=nrow(trial1)))
-            # df <- rbind(df,temp_df)} 
-            # p=ggplot(df,aes(x=x,y=y,group=col,colour=factor(col))) +geom_line()
-            # p=p+guides(colour=guide_legend(ncol=2))
-            # p
+            image <- load.image("/Users/Mac/Desktop/SCV/SCV_project/MATH517team2-2/Wildfires and the land cover analysis/interactiveapp/Rplot_trials.jpeg")
+            plot(image,axes=FALSE, frame.plot=TRUE)
+  
+           
+       
 
         }
 
         )
+            
+           
   
     
       }
